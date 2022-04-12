76bebc500a3ddebe2286595164ca141d22c95502
CalebAHJ <calebharperjohnson@gmail.com>
2022-04-12 15:47:13 -0500

    display function created

--- lib.js
+++ lib.js
@@ -10,3 +10,29 @@ function Book(author, title, pages, beenRead) {
 function addBook (book) {
     library.push(book);
 }
+
+function display () {
+    for (const book of library) {
+        const bookCard = document.createElement('div');
+
+        const box = document.querySelector('#container');
+        box.appendChild(bookCard);
+
+        const author = document.createElement('div');
+        author.textContent = book.author;
+
+        const title = document.createElement('div');
+        title.textContent = book.title;
+
+        const pages = document.createElement('div');
+        pages.textContent = book.pages.toString();
+
+        const beenRead = document.createElement('div'); 
+        beenRead.textContent = book.beenRead;
+
+        bookCard.appendChild(author);
+        bookCard.appendChild(title);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(beenRead);
+    }
+}
